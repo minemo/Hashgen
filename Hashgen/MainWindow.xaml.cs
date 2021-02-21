2b2768d0929f053c6efdf8c5b35f48c874d62322
MinemoTV <wittki56@gmail.com>
2021-02-21 23:57:50 +0100

    Layout Correction

--- Hashgen/MainWindow.xaml.cs
+++ Hashgen/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +13,56 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
 
 namespace Hashgen
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        public string rootpath;
+        public List<String> allhash;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public string rootpath;
+        public static List<String> ProcessDir(string targetdir)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetdir);
+            List<String> hashlist = new List<string>();
+            foreach (string fileName in fileEntries)
+                hashlist.Add(ProcessFile(fileName));
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetdir);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDir(subdirectory);
+
+            return hashlist;
+        }
+
+        public static string ProcessFile(string path)
+        {
+            MD5 alg = MD5.Create();
+            using FileStream stream = File.OpenRead(path);
+            byte[] hashcode = alg.ComputeHash(stream);
+            return BitConverter.ToString(hashcode).Replace("-", "");
+        }
 
         private void openfileselect(object sender, RoutedEventArgs e)
         {
-            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            using var dialog = new System.Windows.Forms.FolderBrowserDialog();
+             System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            rootpath = "Der Festgelegte Pfad ist: " + '"' + dialog.SelectedPath + '"';
+            Lbox.Items.Add(rootpath);
+            allhash = ProcessDir(dialog.SelectedPath);
+            foreach(String hash in allhash)
             {
-                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
-                rootpath = result.ToString();
-                Console.Out.WriteLine(rootpath);
+                Lbox.Items.Add(hash);
             }
+
         }
     }
 }
